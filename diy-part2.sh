16c9848553cd6e103085de5c6203e2f29e748d83
lm1618 <63717679+lm1618@users.noreply.github.com>
2023-05-16 06:04:28 +0800

    Update diy-part2.sh
    5.4

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,4 @@ sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 #修改内核版本
 sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.4/g' target/linux/ramips/Makefile
 
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/ramips/Makefile
